bb1f331432e39b0846fc661a9f12836d5d9311a6
Vlados <vladprkrf@mail.ru>
2024-10-28 09:38:43 +0700

    Задача 5 решена :)

--- task5.py
+++ task5.py
@@ -1,2 +1,16 @@
 # Введите массив длины N с клавиатуры и найдите (за один проход)
 # количество элементов, имеющих максимальное значение.
+n = int(input())
+x = []
+mas_itog = []
+max_znach = -10*100
+for i in range(n):
+    elem = int(input())
+    x.append(elem)
+    if x[i] > max_znach:
+        max_znach = x[i]
+        mas_itog = []
+        mas_itog.append(x[i])
+    if x[i] == max_znach:
+        mas_itog.append(x[i])
+print(len(mas_itog) - 1)
