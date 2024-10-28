cfd7fb493cd183df4d7a28decb31b73c1e1098f3
Vlados <vladprkrf@mail.ru>
2024-10-28 09:50:13 +0700

    Задача 6 решена

--- task6.py
+++ task6.py
@@ -4,3 +4,14 @@
 # Пример: ввод N = 6
 # [1,2,3,4,5,6]
 # Вывод: [3,2,1,6,5,4]
+from random import randint as ran
+n = int(input())
+x = []
+for i in range(n):
+    elem = ran(0, 10)
+    x.append(elem)
+x_ = x[::-1]
+mas_1 = x_[n//2:]
+mas_2 = x_[:n//2]
+print(x)
+print(mas_1 + mas_2)
