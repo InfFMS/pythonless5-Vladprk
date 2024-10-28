cdac1e83d8adee4d94a3ff72f738a1a4d8a216ba
Vlados <vladprkrf@mail.ru>
2024-10-28 10:14:30 +0700

    Задача 8 решена

--- task8.py
+++ task8.py
@@ -4,3 +4,26 @@
 # Пример: ввод N = 4
 # [12, 77, 5555, 97]
 # Вывод: [77, 5555]
+from random import randint as ran
+
+
+def proverka(n):
+    n_ = str(n)
+    for i in range(len(n_)-1):
+        if n_[i] != n_[i+1]:
+            return False
+        else:
+            b = True
+    if b:
+        return True
+
+
+n = int(input())
+x = []
+mad_mas = []
+for i in range(n):
+    elem = ran(0, 100000)
+    x.append(elem)
+    if proverka(elem):
+        mad_mas.append(elem)
+print(mad_mas)
